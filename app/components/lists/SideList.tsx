7cadd4e554c5723d767ca6efc6c7ff5e808a19cc
Neil Wang <neilywang3@gmail.com>
2025-09-07 17:50:35 -0400

    updated general files

---
+++ app/components/lists/SideList.tsx
@@ -0,0 +1,12 @@
+
+import type SideListProps from "./SideListProps";
+
+
+export default function SideList({params}: {params: SideListProps}) {
+    return (
+        <div className="flex flex-1 h-full w-full bg-gray-700 text-white">
+            Side List
+        </div>
+    )
+}
+
--- app/routes/music.tsx
+++ app/routes/music.tsx
@@ -1,35 +1,32 @@
 
-import type { Route } from "./+types/music";
+import SideList from "~/components/lists/SideList";
 
 // TESTING: imports
 import { SERVER_INFO } from "../../.testing/serverInfo";
 
 export default function Music() {
 
-    // TESTING: function calls
-    const testFetch = async () => {
-        try {
-            const data = await fetch(`${SERVER_INFO.URL}/api/data`)
+    // // TESTING: function calls
+    // const testFetch = async () => {
+    //     try {
+    //         const data = await fetch(`${SERVER_INFO.URL}/api/data`)
 
-            if (!data.ok) {
-                throw new Error("Failed to fetch data");
-            }
+    //         if (!data.ok) {
+    //             throw new Error("Failed to fetch data");
+    //         }
 
-            const jsonData = await data.json();
-            console.log(jsonData);
+    //         const jsonData = await data.json();
+    //         console.log(jsonData);
 
-        } catch (e) {
-            console.error(e)
-        }
-    }
+    //     } catch (e) {
+    //         console.error(e)
+    //     }
+    // }
 
     
     return (
-        <>
-            <h1>Music</h1>
-            <button onClick={testFetch}>
-                Fetch Data
-            </button>
-        </>
+        <div className="flex h-full w-full flex-1 flex-row bg-gray-900 text-white">
+            <SideList params={{}} />
+        </div>
     )
 }
